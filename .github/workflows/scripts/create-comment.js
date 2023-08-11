d170df08d1ac8db2604a122767096f580ed5c34f
Abdullah Atta <abdullahatta@streetwriters.co>
2023-08-11 15:38:51 +0500

    ci: use actions/github-script for validation & comments

---
+++ .github/workflows/scripts/create-comment.js
@@ -0,0 +1,39 @@
+const MESSAGE_ID = `<!-- add-pr-comment:add-pr-comment -->`;
+module.exports = async ({ github, context, body }) => {
+  const parameters = {
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    issue_number: context.issue.number,
+    per_page: 100,
+  };
+
+  let found;
+  for await (const comments of github.paginate.iterator(
+    github.rest.issues.listComments,
+    parameters
+  )) {
+    found = comments.data.find(({ body }) => {
+      return (body?.search(MESSAGE_ID) ?? -1) > -1;
+    });
+
+    if (found) {
+      break;
+    }
+  }
+
+  if (found) {
+    await octokit.rest.issues.deleteComment({
+      comment_id: found.id,
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      body: found.body,
+    });
+  }
+
+  await github.rest.issues.createComment({
+    issue_number: context.issue.number,
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    body: `${MESSAGE_ID}\n\n${body}`,
+  });
+};
