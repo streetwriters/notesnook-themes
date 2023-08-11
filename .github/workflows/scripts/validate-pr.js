1ced4056be68322a49c88cf8d0809a24f11645f5
Abdullah Atta <abdullahatta@streetwriters.co>
2023-08-11 22:47:13 +0500

    ci: make sure theme file is named theme.json

--- .github/workflows/scripts/validate-pr.js
+++ .github/workflows/scripts/validate-pr.js
@@ -1,9 +1,16 @@
 const { readFile } = require("fs/promises");
 const comment = require("./create-comment");
+const path = require("path");
 
 module.exports = async ({ github, context, files }) => {
   const errors = [];
   for (const file of files.split("\n")) {
+    if (path.basename(file) !== "theme.json") {
+      errors.push(
+        "The JSON file must be named theme.json otherwise it won't work."
+      );
+    }
+
     const theme = JSON.parse(await readFile(file, "utf-8"));
     const expectedPath = `themes/${theme.id}/v${theme.compatibilityVersion}/theme.json`;
     if (file !== expectedPath) {
